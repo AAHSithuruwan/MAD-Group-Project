2395f87401fb72e0b92c7760f104606bf4e309f6
Hansa Sithuruwan <sithuruwan2002@gmail.com>
2025-02-11 13:39:14 +0530

    Created Add Items Page

--- mad_group_project/lib/main.dart
+++ mad_group_project/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mad_group_project/add_items.dart';
 import 'package:mad_group_project/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home()
+    home: AddItems()
   ));
 }
 
