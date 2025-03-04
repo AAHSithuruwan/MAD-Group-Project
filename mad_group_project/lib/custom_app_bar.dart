4f31f340e57b4ecada6c4d0877dc84a1913087d7
Hansa Sithuruwan <sithuruwan2002@gmail.com>
2025-03-04 17:31:03 +0530

    Adding welcome page

--- mad_group_project/lib/custom_app_bar.dart
+++ mad_group_project/lib/custom_app_bar.dart
@@ -20,6 +20,7 @@ class _CustomAppBarState extends State<CustomAppBar> {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      automaticallyImplyLeading: false,
       title: IconButton(
           onPressed: (){
             Navigator.pop(context);
