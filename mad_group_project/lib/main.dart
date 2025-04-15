8339844391a15615c4b6845cdb4bbb287b57538c
Kasun-Abeywickrama <kasunthiwanka158@gmail.com>
2025-04-15 07:42:31 +0530

    Integrate the firebase

--- mad_group_project/lib/main.dart
+++ mad_group_project/lib/main.dart
@@ -1,18 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mad_group_project/add_items.dart';
-import 'package:mad_group_project/home.dart';
-import 'package:mad_group_project/main_container.dart';
-import 'package:mad_group_project/register.dart';
-import 'package:mad_group_project/signIn.dart';
-import 'package:mad_group_project/sign_in_selection.dart';
-import 'package:mad_group_project/sign_up_selection.dart';
-import 'package:mad_group_project/verifyEmail.dart';
+import 'package:mad_group_project/firebase_options.dart';
 import 'package:mad_group_project/welcome.dart';
-import 'package:mad_group_project/create_items.dart';
-import 'package:mad_group_project/quantity_selection.dart';
 
-import 'forgotPassword.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Correct class name.
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // runApp(MaterialApp(home: SignInSelection()));
   runApp(MaterialApp(home: Welcome()));
 }
