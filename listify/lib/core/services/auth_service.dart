cfd6c9b4f7bd74beefdf510ef293cc3241f6616c
Kasun-Abeywickrama <kasunthiwanka158@gmail.com>
2025-05-04 19:26:53 +0530

    Create auth_service.dart

---
+++ listify/lib/core/services/auth_service.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign up with email and password
+  Future<User?> signUpWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email.trim(), password: password);
+      return result.user;
+    } catch (e) {
+      print("Error signing up: $e");
+      return null; // Or handle the error appropriately
+    }
+  }
+
+  // Sign in with email and password
+  Future<User?> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email.trim(), password: password);
+      return result.user;
+    } catch (e) {
+      print("Error signing in: $e");
+      return null; // Or handle the error appropriately
+    }
+  }
+
+  // Get the current user
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
+
+  // Sign out
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
