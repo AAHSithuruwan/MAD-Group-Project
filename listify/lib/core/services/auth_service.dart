071bbbb4f231cb4662385fad44ef239fcea3ce22
Kasun-Abeywickrama <kasunthiwanka158@gmail.com>
2025-05-05 12:55:11 +0530

    Upload forgot password logics with firebase operations

--- listify/lib/core/services/auth_service.dart
+++ listify/lib/core/services/auth_service.dart
@@ -32,6 +32,11 @@ class AuthService {
     }
   }
 
+  // Send password reset email
+  Future<void> sendPasswordResetEmail(String email) async {
+    await _auth.sendPasswordResetEmail(email: email.trim());
+  }
+
   // Sign out
   Future<void> signOut() async {
     await _auth.signOut();
--- listify/lib/router/app_router.dart
+++ listify/lib/router/app_router.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart'; 
-import 'package:go_router/go_router.dart'; 
-import 'package:listify/core/providers/auth_provider.dart'; 
-import '../features/auth/presentation/screens/auth_selection.dart'; 
-import '../features/auth/presentation/screens/sign_in_screen.dart'; 
-import '../features/auth/presentation/screens/sign_up_screen.dart'; 
-import '../features/home/presentation/screens/home.dart'; 
-import './initial_route_helper.dart'; 
-import '../features/welcome/presentation/screens/welcome_screen.dart'; 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:listify/core/providers/auth_provider.dart';
+import 'package:listify/features/auth/presentation/screens/forgot_password.dart';
+import '../features/auth/presentation/screens/auth_selection.dart';
+import '../features/auth/presentation/screens/sign_in_screen.dart';
+import '../features/auth/presentation/screens/sign_up_screen.dart';
+import '../features/home/presentation/screens/home.dart';
+import './initial_route_helper.dart';
+import '../features/welcome/presentation/screens/welcome_screen.dart';
 
 class AppRouter {
   // Define route paths
@@ -16,6 +17,7 @@ class AppRouter {
   static const String authSelectionRoute = '/auth-selection';
   static const String signInRoute = '/sign-in';
   static const String signUpRoute = '/sign-up';
+  static const String forgotPasswordRoute = '/forgot-password';
 
   // List of protected routes (requires user to be signed in)
   static final List<GoRoute> protectedRoutes = [
@@ -30,8 +32,10 @@ class AppRouter {
   // Create the router with authentication and redirection logic
   static Future<GoRouter> createRouter(WidgetRef ref) async {
     // Determine the initial route (e.g., home or welcome)
-    final initialRoute =
-        await InitialRouteHelper.getInitialRoute(homeRoute, welcomeRoute);
+    final initialRoute = await InitialRouteHelper.getInitialRoute(
+      homeRoute,
+      welcomeRoute,
+    );
 
     return GoRouter(
       initialLocation: initialRoute, // Set the initial route
@@ -40,8 +44,9 @@ class AppRouter {
         final authState = ref.read(authStateProvider).asData?.value;
 
         // Check if the user is trying to access a protected route
-        final isAccessingProtectedRoute =
-            protectedRoutes.any((route) => state.uri.toString() == route.path);
+        final isAccessingProtectedRoute = protectedRoutes.any(
+          (route) => state.uri.toString() == route.path,
+        );
 
         // Redirect to auth-selection if not signed in and accessing a protected route
         if (isAccessingProtectedRoute && authState == null) {
@@ -55,32 +60,43 @@ class AppRouter {
         GoRoute(
           path: welcomeRoute,
           name: 'welcome',
-          builder: (BuildContext context, GoRouterState state) =>
-              const WelcomeScreen(),
+          builder:
+              (BuildContext context, GoRouterState state) =>
+                  const WelcomeScreen(),
         ),
 
         // Auth selection screen route
         GoRoute(
           path: authSelectionRoute,
           name: 'auth_selection',
-          builder: (BuildContext context, GoRouterState state) =>
-              const AuthSelection(),
+          builder:
+              (BuildContext context, GoRouterState state) =>
+                  const AuthSelection(),
         ),
 
         // Sign-up screen route
         GoRoute(
           path: signUpRoute,
           name: "sign_up",
-          builder: (BuildContext context, GoRouterState state) =>
-              SignUpScreen(),
+          builder:
+              (BuildContext context, GoRouterState state) => SignUpScreen(),
         ),
 
         // Sign-in screen route
         GoRoute(
           path: signInRoute,
           name: 'sign_in',
-          builder: (BuildContext context, GoRouterState state) =>
-              SignInScreen(),
+          builder:
+              (BuildContext context, GoRouterState state) => SignInScreen(),
+        ),
+
+        // Forgot Password screen route
+        GoRoute(
+          path: forgotPasswordRoute,
+          name: 'forgot_password',
+          builder:
+              (BuildContext context, GoRouterState state) =>
+                  ForgotPasswordScreen(),
         ),
 
         // Include all protected routes
